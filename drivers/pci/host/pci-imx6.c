9bc0e787158a205e74392e30cd7a8e8705046b02
Your Name <Your Email>
2016-06-30 17:08:25 +0200

    0027-add-icorem6sx-support-011

--- drivers/pci/host/pci-imx6.c
+++ drivers/pci/host/pci-imx6.c
@@ -10,7 +10,7 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-
+#define DEBUG
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
@@ -370,27 +370,19 @@ static int imx6_pcie_deassert_core_reset(struct pcie_port *pp)
 		dev_err(pp->dev, "unable to enable pcie clock\n");
 		goto err_pcie;
 	}
-
+/*
 	if (!IS_ENABLED(CONFIG_EP_MODE_IN_EP_RC_SYS)
 			&& !IS_ENABLED(CONFIG_RC_MODE_IN_EP_RC_SYS)) {
-		if (imx6_pcie->ext_osc) {
-			clk_set_parent(imx6_pcie->pcie_ext,
-					imx6_pcie->pcie_ext_src);
-			ret = clk_prepare_enable(imx6_pcie->pcie_ext);
-			if (ret) {
-				dev_err(pp->dev, "unable to enable pcie_ext clock\n");
-				goto err_pcie_bus;
-			}
-		} else {
-			ret = clk_prepare_enable(imx6_pcie->pcie_bus);
-			if (ret) {
-				dev_err(pp->dev, "unable to enable pcie_bus clock\n");
-				goto err_pcie_bus;
-			}
+*/
+		ret = clk_prepare_enable(imx6_pcie->pcie_bus);
+		if (ret) {
+			dev_err(pp->dev, "unable to enable pcie_bus clock\n");
+			goto err_pcie_bus;
 		}
+/*
 	}
-
-	ret = clk_prepare_enable(imx6_pcie->pcie_phy);
+*/
+	ret = clk_prepare_enable(imx6_pcie->pcie);
 	if (ret) {
 		dev_err(pp->dev, "unable to enable pcie_phy clock\n");
 		goto err_pcie_phy;
